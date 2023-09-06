d1642909348ef4ecd89b7a1710028f259a01a6a9
Heyoon <parechae@gmail.com>
2023-09-07 01:32:35 +0900

    docs : "PooingManager주석추가"

--- AllStar/Assets/02.Scripts/Manager/PoolingManager.cs
+++ AllStar/Assets/02.Scripts/Manager/PoolingManager.cs
@@ -62,26 +62,31 @@ public class PoolingManager
     Dictionary<string, Pool> _pools = new Dictionary<string, Pool>();
     public GameObject Pop(GameObject prefab)
     {
-        if (_pools.ContainsKey(prefab.name) == false)
-            CreatePool(prefab);
-
+        //매개변수 prefab을 받아서 같은 게임오브젝트 pool이 없다면! 풀을 만들어준다
+        if (_pools.ContainsKey(prefab.name) == false)CreatePool(prefab);
+        //매개변수로 받는 게임오브젝트와 같은 이름을 가진 오브젝트를 _pools에서 뽑아준다
         return _pools[prefab.name].Pop();
     }
     public bool Push(GameObject go)
     {
         if (_pools.ContainsKey(go.name) == false)
         {
+            //_pools에 매개변수로 받은 오브젝트와 같은 이름을 가진풀이 없으면 false를 반환
             return false;
         }
+        //게임오브젝트를 풀로 반환해준다
         _pools[go.name].Push(go);
+        //_pools에 매개변수로 받은 오브젝트와 같은 이름을 가진풀이 없으면 true를 반환
         return true;
     }
     public void Clear()
     {
+        //풀을 비워줌
         _pools.Clear();
     }
     void CreatePool(GameObject original)
     {
+        //새로운 풀을 등록
         Pool pool = new Pool(original);
         _pools.Add(original.name, pool);
     }
