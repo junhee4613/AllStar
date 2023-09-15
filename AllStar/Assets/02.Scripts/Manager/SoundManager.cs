44bd0b7eed91f487a82bb0195ac57ac4c45bbdc1
junhee4613 <junhee000123@naver.com>
2023-09-15 20:04:25 +0900

    feat : AudioMixer, Boss패턴스크립트, SoundManager
    Boss패턴은 추상화해서 대체재로 큐브의 회전,스케일, 이동으로 대체
    SoundManager는 아직 작업중

---
+++ AllStar/Assets/02.Scripts/Manager/SoundManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+public class SoundManager : MonoBehaviour
+{
+    public List<AudioClip> bgmList = new List<AudioClip>();
+    public static SoundManager Instance { get; private set; }
+    public AudioSource bgm_Sound;
+    public AudioMixer mixer;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+            DontDestroyOnLoad(gameObject);
+            SceneManager.sceneLoaded += SceneLoadedSetting;
+        }
+    }
+    public void SFX_Sound(GameObject go)
+    {
+        go = new GameObject();
+        AudioSource temp = go.AddComponent<AudioSource>();
+        temp.loop = false;
+        temp.Play();
+        Destroy(go, temp.clip.length);
+    }
+    public void BGM_Sound(Scene arg)
+    {
+        for (int i = 0; i < bgmList.Count; i++)
+        {
+            if(bgmList[i].name == arg.name)
+            {
+                bgm_Sound.clip = bgmList[i];
+            }
+        }
+    }
+    public void SceneLoadedSetting(Scene arg, LoadSceneMode arg1)
+    {
+        BGM_Sound(arg);
+    }
+}
---
+++ AllStar/Assets/02.Scripts/Monster/Boss.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public bool pattern_Start_bool = true;              //패턴이 시작하기 위한 불값
+    public float standby_time;                          //모든 패턴 중지 시간
+    public string motion_Type;                          //랜덤한 패턴을 시작하기 위한 string값
+    public int randomNum;
+    public BossMotion motion;
+    public enum BossMotion
+    {
+        STOP,
+        ROTATE,
+        MOVE,
+        SCALE_UP,
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pattern_Start_bool)
+        {
+            standby_time += Time.deltaTime;
+            if (standby_time > randomNum)
+                Pattern();
+        }
+    }
+    public void Pattern()
+    {
+        pattern_Start_bool = false;         
+        randomNum = Random.Range(0, 4);
+        motion_Type = $"Motion{randomNum}";
+        motion = (BossMotion)randomNum;
+        switch (motion)
+        {
+            case (BossMotion)0: //가만히 있기
+                StartCoroutine(motion_Type);
+                break;
+            case (BossMotion)1:
+                StartCoroutine(motion_Type);
+                break;
+            case (BossMotion)2:
+                StartCoroutine(motion_Type);
+                break;
+            case (BossMotion)3:
+                StartCoroutine(motion_Type);
+                break;
+            default:
+                break;
+
+
+        }
+    }
+    IEnumerator Motion0()
+    {
+        yield return new WaitForSeconds(1);
+        standby_time = 0;
+        pattern_Start_bool = true;
+    }
+    IEnumerator Motion1()
+    {
+        standby_time = 0;
+        while (standby_time < randomNum + 2)
+        {
+            standby_time += Time.deltaTime;
+            gameObject.transform.Rotate(360 * Time.deltaTime, 360 * Time.deltaTime, 360 * Time.deltaTime) ;
+            yield return null;
+        }
+
+        standby_time = 0;
+        pattern_Start_bool = true;
+    }
+    IEnumerator Motion2()
+    {
+        while (standby_time < randomNum + 1)
+        {
+            standby_time += Time.deltaTime;
+            gameObject.transform.position += Vector3.one * Time.deltaTime;
+            yield return null;
+        }
+        
+        standby_time = 0;
+        pattern_Start_bool = true;
+    }
+    IEnumerator Motion3()
+    {
+        while (standby_time < randomNum)
+        {
+            standby_time += Time.deltaTime;
+            gameObject.transform.localScale += Vector3.one * Time.deltaTime;
+            yield return null;
+        }
+        standby_time = 0;
+        pattern_Start_bool = true;
+    }
+}
