e69664be873da5e2497b1497d9a63a3f88e07675
heoyoon <parechae@gmail.com>
2023-09-21 10:49:47 +0900

    feat : 총알 타입 설정을 위한 상태값세분화

--- AllStar/Assets/02.Scripts/PlayerInput/Bullet/BulletBase.cs
+++ AllStar/Assets/02.Scripts/PlayerInput/Bullet/BulletBase.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletBase : MonoBehaviour
+public abstract class BulletBase : MonoBehaviour
 {
-    public float bulletSpeed = 40;
-    public float removeTimer = 0;
+    public float bulletSpeed;
+    public float removeTimer;
     public float bulletDamage;
-    public void setValue()
+    public Mesh bulletModel;
+    public Material bulletMat;
+    [Header("투사체 타입")]
+    public BulletType bulletType;
+    public AttackTypeStatus bulletAttackStatus;
+    [Header("발사 타입")]
+    public ShotType shotType;
+    public ShotStatus shotStatus;
+    //세팅 
+    public virtual void SetBasicValue()
     {
-
+        Debug.Log(this.GetType());
+        //확인 결과 클래스 이름을 가져올 수 있으니 데이터테이블에 총알 스텟을 딕셔너리화 하여 총알 클래스 이름을 기획서와 맞춰 데이터를 불러는식으로 진행
     }
     private void Update()
     {
@@ -27,11 +37,13 @@ public class BulletBase : MonoBehaviour
         {
             removeTimer = 0;
             Managers.Pool.Push(this.gameObject);
+            
         }
         removeTimer += Time.deltaTime;
     }
-    private void Start()
-    {
-        Debug.Log(transform.forward);
-    }
+}
+
+public enum ShotType
+{
+    multiShot,singleShot
 }
